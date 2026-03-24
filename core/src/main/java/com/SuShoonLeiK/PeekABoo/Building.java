9216aefd96e1a40d5c6c6cb05ae9aea1fc61ce26
Su Shoon Lei Khaing <skhaing@oswego.edu>
2026-03-24 10:04:33 -0400

    Refactor Building class color constants

--- core/src/main/java/com/SuShoonLeiK/PeekABoo/Building.java
+++ core/src/main/java/com/SuShoonLeiK/PeekABoo/Building.java
@@ -1,10 +1,16 @@
 package com.SuShoonLeiK.PeekABoo;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.Color;
 
 public class Building {
 
     public float x, y, width, height;
+
+    public static final Color FILL   = new Color(0.20f, 0.20f, 0.23f, 1f);
+    public static final Color BORDER = new Color(0.85f, 0.85f, 0.85f, 1f);
+    public static final float WIDTH  = 120f;
+    public static final float HEIGHT =  90f;
 
     public Building(float x, float y, float width, float height) {
         this.x = x;  this.y = y;
@@ -12,12 +18,12 @@ public class Building {
     }
 
     public void renderFilled(ShapeRenderer sr) {
-        sr.setColor(GameConfig.BUILDING_FILL);
+        sr.setColor(Building.FILL);
         sr.rect(x, y, width, height);
     }
 
     public void renderOutline(ShapeRenderer sr) {
-        sr.setColor(GameConfig.BUILDING_BORDER);
+        sr.setColor(Building.BORDER);
         sr.rect(x, y, width, height);
     }
 
